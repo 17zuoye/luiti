92d91631ee3cb50792d0b6d68c3ec2102db69dbb
David Chen <mvjome@gmail.com>
2015-01-16 12:07:45 +0800

    remove prefix in TaskUtils

--- luiti/utils/task_utils.py
+++ luiti/utils/task_utils.py
@@ -11,7 +11,7 @@ class TaskUtils:
         import string
         assert task_name_1[0] in string.uppercase, "Task name should begin with UpperCase !"
 
-        # 2. check `task_name_1` format.
-        lib1  = importlib.import_module("luiti_tasks." + Inflector().underscore(task_name_1))
+        # 2. import it!
+        lib1  = importlib.import_module(Inflector().underscore(task_name_1))
         task1 = getattr(lib1, task_name_1)
         return task1
