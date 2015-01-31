06059c8979a41ee06a25aebedeb0add7e72923d9
David Chen <mvjome@gmail.com>
2015-01-31 14:34:24 +0800

    Fix mongo_import_task typo

--- luiti/__init__.py
+++ luiti/__init__.py
@@ -55,7 +55,7 @@ from .task_range_hadoop    import TaskRangeHadoop
 
 from .root_task            import RootTask
 from .static_file          import StaticFile
-from .mongo_task           import MongoImportTask
+from .mongo_import_task    import MongoImportTask
 
 from .                     import manager
 from .utils                import IOUtils, DateUtils, TargetUtils, HDFSUtils, \
