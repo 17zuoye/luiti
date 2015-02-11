80a3f05be762bf0267fa0581f0f1119190c888de
David Chen <mvjome@gmail.com>
2015-02-11 10:32:28 +0800

    Extract modules from `luiti` command

--- luiti/manager/__init__.py
+++ luiti/manager/__init__.py
@@ -9,6 +9,8 @@ from .config              import luiti_config
 from .package_map         import PackageMap
 from .setup_packages      import setup_packages
 
+from .sys_argv            import SysArgv
+
 
 
 #################
@@ -23,3 +25,15 @@ load_a_task_by_name                               = Loader.load_a_task_by_name
 import2                                           = luiti_config.import2
 print_all_tasks                                   = Table.print_all_tasks
 print_files_by_task_cls_and_date_range            = Table.print_files_by_task_cls_and_date_range
+
+
+from etl_utils import singleton, cached_property
+@singleton()
+class LazyData(object):
+
+    @cached_property
+    def all_task_classes(self): return [i1['task_cls'] for i1 in ld.result['success']]
+
+    @cached_property
+    def result(self): return load_all_tasks()
+ld = LazyData()
