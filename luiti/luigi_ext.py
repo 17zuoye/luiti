028923fafb53121ee706edb2842c211aca83079b
David Chen <mvjome@gmail.com>
2015-03-18 18:59:55 +0800

    Add some notes about luigi.plug_packages

--- luiti/luigi_ext.py
+++ luiti/luigi_ext.py
@@ -192,12 +192,15 @@ def plug_packages(*package_names):
     Let luigi know which packages should be attached, and can send to YARN, etc.
 
     Package format can be any valid Python package name, such as "project_B" or "project_C==0.0.2", etc.
+
+    Usage: use `active_packages` decorator to notice luigi that these packages should include.
     """
     if len(manager.luiti_config.attached_package_names) > 1:
         return False # default is luiti. and can plug only once.
 
     for p1 in package_names:
         manager.luiti_config.attached_package_names.add(p1)
+# TODO why should do `luigi.hadoop.attach` in `active_packages`
 luigi.plug_packages = plug_packages
 
 
