7874323f0ed2fe7fe8226a7b722f9f48b0c7a431
David Chen <mvjome@gmail.com>
2015-03-30 18:22:02 +0800

    Make TaskWeek#days_in_week cachable

--- luiti/task_templates/time/task_week.py
+++ luiti/task_templates/time/task_week.py
@@ -3,11 +3,12 @@
 __all__ = ['TaskWeek']
 
 from .task_base import TaskBase
-from ...utils  import DateUtils
+from ...utils   import DateUtils
+from etl_utils  import cached_property
 
 
 class TaskWeek(TaskBase):
 
-    @property
+    @cached_property
     def days_in_week(self):
         return list(DateUtils.days_in_week(self.date_value))
