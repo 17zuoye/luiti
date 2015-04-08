460adc2e97ce6f6ea5ac6d97ec0170bcc7e74c99
David Chen <mvjome@gmail.com>
2015-04-08 09:49:45 +0800

    active_packages compact with version, with tests passed

--- luiti/manager/active_packages.py
+++ luiti/manager/active_packages.py
@@ -21,7 +21,10 @@ def active_packages(orig_func):
                 continue
             else:
                 # Pip cant manage versions packages, only exist one version at one time.
-                pkg_resources.require(package2)
+                try:
+                    if version2: pkg_resources.require(p1)
+                except:
+                    pkg_resources.require(package2)
 
                 # TODO luiti 拷之前需要版本，之后不需要，分布式时判断目录packages即可。
                 # Notice Python to import special version package.
