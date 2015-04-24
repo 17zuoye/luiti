bbe8235f7168d10a7070b3ab761262273dbc1625
David Chen <mvjome@gmail.com>
2015-04-24 14:45:50 +0800

    clean IOUtils

--- luiti/utils/io_utils.py
+++ luiti/utils/io_utils.py
@@ -1,9 +1,12 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 import json
-import luigi, luigi.hdfs
+import luigi
+import luigi.hdfs
 from luigi import LocalTarget
 from etl_utils import JsonUtils
+from .target_utils import TargetUtils
+
 
 class IOUtils:
 
@@ -12,23 +15,30 @@ class IOUtils:
     @staticmethod
     def write_json_to_output(json1, output1):
         with output1.open('w') as output_hdfs:
-            if isinstance(json1, (list,set,)):
-                method = lambda item1: json.dumps(list(item1)) # 兼容 JsonUtils.unicode_dump 不支持list
+            m1 = lambda item1: json.dumps(list(item1))
+            m2 = lambda item1: JsonUtils.unicode_dump(item1).encode("UTF-8")
+            if isinstance(json1, (list, set,)):
+                # 兼容 JsonUtils.unicode_dump 不支持list
+                method = m1
             else:
-                method = lambda item1: JsonUtils.unicode_dump(item1).encode("UTF-8")
+                method = m2
             output_hdfs.write(method(json1) + "\n")
 
     @staticmethod
     def read_json_from_output(output1):
         # only one line
         item1 = None
-        with output1.open('r') as output_hdfs:
-            for line1 in output_hdfs:
-                item1 = json.loads(line1)
+        read_line_count = 0
+        for json1 in TargetUtils.json_read(output1):
+            read_line_count += 1
+            item1 = json1
+            if read_line_count >= 2:
+                raise ValueError("[multiple line error]"
+                                 " %s should contain only one line!" % output1)
         return item1
 
     @staticmethod
-    def remove_files(*files): # 兼容 写入中途失败
+    def remove_files(*files):  # 兼容 写入中途失败
         for file1 in files:
             if luigi.hdfs.exists(file1):
                 luigi.hdfs.remove(file1)
