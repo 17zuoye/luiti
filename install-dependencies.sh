f1c59d910f6b362d1aba50991c84f5723c2159d3
David Chen <mvjome@gmail.com>
2015-06-24 18:36:00 +0800

    Fix pip install pyparsing --retries 10 --timeout 60

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,4 +3,7 @@ npm install bower -g
 cd luiti/webui; bower install; cd -;
 
 # Install eggs dependencies.
+
+# Fix => Reading http://pyparsing.wikispaces.com/ error: timed out
+pip install pyparsing --retries 10 --timeout 60
 python setup.py install
