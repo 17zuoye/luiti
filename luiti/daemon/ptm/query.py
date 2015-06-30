6e675044475bc9e948f978b0f69b4443f54e618c
David Chen <mvjome@gmail.com>
2015-06-30 15:01:22 +0800

    Refactor luiti/daemon/ptm/query.py with QueryBuilder

--- luiti/daemon/ptm/query.py
+++ luiti/daemon/ptm/query.py
@@ -2,8 +2,148 @@
 
 __all__ = ["Query"]
 
+from etl_utils import cached_property
+
 from ..graph import Graph
 from ..template import Template
+
+
+# TODO more modular
+class QueryBuilder(object):
+    """
+    Construct a query builder.
+
+    All propertyies are generated lazily by using `cached_property`, as in a DAG.
+    """
+
+    def __init__(self, ptm, raw_params):
+        assert isinstance(raw_params, dict), raw_params
+
+        self.raw_params = raw_params
+        self.ptm = ptm
+
+    @cached_property
+    def query_params(self):
+        return self.ptm.generate_query_params()
+
+    @cached_property
+    def selected_query(self):
+        return self.ptm.generate_selected_query(self.default_query,
+                                                self.raw_params,
+                                                self.selected_packages)
+
+    @cached_property
+    def default_query(self):
+        return self.ptm.generate_default_query(self.query_params)
+
+    @cached_property
+    def default_packages(self):
+        return self.ptm.current_luiti_visualiser_env["package_config"]["defaults"]
+
+    @cached_property
+    def selected_packages(self):
+        return self.raw_params.get("luiti_package", self.default_packages)
+
+    @cached_property
+    def selected_task_cls_names(self):
+        result = set(self.raw_params.get("task_cls", []))
+
+        # modify other cached_property
+        self.selected_query["task_cls"] = list(result)
+
+        return result
+
+    @cached_property
+    def total_task_instances(self):
+        return self.ptm.generate_total_task_instances(self.default_query,
+                                                      self.selected_query,
+                                                      self.ptm.task_class_names)
+
+    @cached_property
+    def selected_task_instances(self):
+        # TODO filter nodes
+        result = filter(lambda ti: ti.package_name in self.selected_packages,
+                        self.total_task_instances)
+        result = sorted(list(set(result)))
+
+        # To avoid only self is in the graph.
+        # If select task class, then to find linked task instances.
+        if self.selected_task_cls_names:
+            pure_selected_task_instances = [ti for ti in result if ti.task_clsname in self.selected_task_cls_names]
+            pure_linked = set([])
+            for ti in pure_selected_task_instances:
+                for t2 in self.graph_infos_python["requires"]["total"][ti]:
+                    pure_linked.add(t2)
+                for t2 in self.graph_infos_python["upons"]["total"][ti]:
+                    pure_linked.add(t2)
+            # filter that tasks are linked, in current task_classes.
+            result = [ti for ti in result if ti in pure_linked]
+            result.extend(pure_selected_task_instances)
+            result = list(set(result))
+        return result
+
+    @cached_property
+    def graph_infos_data(self):
+        return Graph.analysis_dependencies_between_nodes(self.total_task_instances,
+                                                         self.selected_packages)
+
+    @cached_property
+    def graph_infos_python(self):
+        return self.graph_infos_data["python"]
+
+    @cached_property
+    def nodes(self):
+        return [Template.a_node(ti) for ti in self.selected_task_instances]
+
+    @cached_property
+    def nodeid_to_node_dict(self):
+        return {node["id"]: node for node in self.nodes}
+
+    @cached_property
+    def edges(self):
+        return Template.edges_from_task_instances(self.selected_task_instances)
+
+    @cached_property
+    def nodes_groups(self):
+        return Graph.split_edges_into_groups(self.edges,
+                                             self.nodes,
+                                             self.selected_task_instances)
+
+    @cached_property
+    def nodes_groups_in_view(self):
+        return [sorted(list(nodes_set)) for nodes_set in self.nodes_groups]
+
+    @cached_property
+    def task_instance_repr_to_info(self):
+        return self.ptm.generate_task_instance_repr_to_info(self.selected_task_instances)
+
+    @cached_property
+    def result(self):
+        return {
+            "query_params": self.query_params,
+
+            "title": "A DAG timely visualiser.",
+            "selected_query": self.selected_query,
+            "default_query": self.default_query,
+            "luiti_visualiser_env": self.ptm.current_luiti_visualiser_env,
+
+            "task_class_names": self.ptm.task_class_names,
+            "task_package_names": self.ptm.task_package_names,
+            "task_clsname_to_package_name": self.ptm.task_clsname_to_package_name,
+            "package_to_task_clsnames": self.ptm.package_to_task_clsnames,
+
+            "nodes": self.nodes,
+            "edges": self.edges,
+            "nodes_groups": self.nodes_groups_in_view,
+            "nodeid_to_node_dict": self.nodeid_to_node_dict,
+
+            "graph_infos": self.graph_infos_data["json"],
+            "task_instance_repr_to_info": self.task_instance_repr_to_info,
+
+            "errors": {
+                "load_tasks": self.ptm.load_all_tasks_result["failure"],
+            }
+        }
 
 
 class Query(object):
@@ -15,75 +155,7 @@ class Query(object):
         """
         Generate all data needed.
         """
-        query_params = self.generate_query_params()
-        default_query = self.generate_default_query(query_params)
-
-        default_packages = self.current_luiti_visualiser_env["package_config"]["defaults"]
-
-        selected_packages = raw_params.get("luiti_package", default_packages)
-        selected_task_cls_names = set(raw_params.get("task_cls", []))
-        selected_task_cls_names  # s
-
-        selected_query = self.generate_selected_query(default_query, raw_params, selected_packages)
-
         # total_task_instances = self.generate_total_task_instances(default_query, selected_query, selected_task_cls_names)
-        # TODO filter nodes
-        total_task_instances = self.generate_total_task_instances(default_query, selected_query, self.task_class_names)
-        graph_infos_data = Graph.analysis_dependencies_between_nodes(total_task_instances, selected_packages)
-        graph_infos_python = graph_infos_data["python"]
 
-        selected_task_instances = filter(lambda ti: ti.package_name in selected_packages, total_task_instances)
-        selected_task_instances = sorted(list(set(selected_task_instances)))
-
-        # To avoid only self is in the graph.
-        # If select task class, then to find linked task instances.
-        if selected_task_cls_names:
-            pure_selected_task_instances = [ti for ti in selected_task_instances if ti.task_clsname in selected_task_cls_names]
-            pure_linked = set([])
-            for ti in pure_selected_task_instances:
-                for t2 in graph_infos_python["requires"]["total"][ti]:
-                    pure_linked.add(t2)
-                for t2 in graph_infos_python["upons"]["total"][ti]:
-                    pure_linked.add(t2)
-            # filter that tasks are linked, in current task_classes.
-            selected_task_instances = [ti for ti in selected_task_instances if ti in pure_linked]
-            selected_task_instances.extend(pure_selected_task_instances)
-            selected_task_instances = list(set(selected_task_instances))
-
-        nodes = ([Template.a_node(ti) for ti in selected_task_instances])
-        nodeid_to_node_dict = {node["id"]: node for node in nodes}
-
-        edges = Template.edges_from_task_instances(selected_task_instances)
-
-        nodes_groups = Graph.split_edges_into_groups(edges, nodes, selected_task_instances)
-        nodes_groups_in_view = [sorted(list(nodes_set)) for nodes_set in nodes_groups]
-
-        task_instance_repr_to_info = self.generate_task_instance_repr_to_info(selected_task_instances)
-
-        selected_query["task_cls"] = list(selected_task_cls_names)
-
-        return {
-            "query_params": query_params,
-
-            "title": "A DAG timely visualiser.",
-            "selected_query": selected_query,
-            "default_query": default_query,
-            "luiti_visualiser_env": self.current_luiti_visualiser_env,
-
-            "task_class_names": self.task_class_names,
-            "task_package_names": self.task_package_names,
-            "task_clsname_to_package_name": self.task_clsname_to_package_name,
-            "package_to_task_clsnames": self.package_to_task_clsnames,
-
-            "nodes": nodes,
-            "edges": edges,
-            "nodes_groups": nodes_groups_in_view,
-            "nodeid_to_node_dict": nodeid_to_node_dict,
-
-            "graph_infos": graph_infos_data["json"],
-            "task_instance_repr_to_info": task_instance_repr_to_info,
-
-            "errors": {
-                "load_tasks": self.load_all_tasks_result["failure"],
-            }
-        }
+        qb = QueryBuilder(self, raw_params)
+        return qb.result
