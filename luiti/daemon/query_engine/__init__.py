6e874bf8ee43165e00066b2674141d3e54e75229
David Chen <mvjome@gmail.com>
2015-07-01 17:46:32 +0800

    Add a simple cache to QueryEngine

--- luiti/daemon/query_engine/__init__.py
+++ luiti/daemon/query_engine/__init__.py
@@ -10,6 +10,8 @@ class Query(object):
     Use params to query some data from luiti.
     """
 
+    cache = dict()
+
     def __init__(self, ptm):
         self.ptm = ptm  # global task and package data.
 
@@ -17,5 +19,12 @@ class Query(object):
         """
         Generate all data needed.
         """
-        qb = QueryBuilder(self.ptm, raw_params)
-        return qb.result
+        # TODO cache maybe replaced by a decorator, such as @functools.lru_cache
+        cache_key = unicode(sorted(raw_params.items()))  # A simple cache
+
+        result = self.cache.get(cache_key, None)
+        if result is None:
+            result = QueryBuilder(self.ptm, raw_params).result
+            self.cache[cache_key] = QueryBuilder(self.ptm, raw_params).result
+
+        return result
