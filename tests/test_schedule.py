e40a93948e5ddd1bbe0392a455991b8ff282d2ba
David Chen <mvjome@gmail.com>
2015-07-10 12:26:37 +0800

    Fix time local at test_read_all_required_tasks

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -11,19 +11,16 @@ from luiti.tests import SetupLuitiPackages
 config = SetupLuitiPackages.config
 
 from luiti.schedule import SensorSchedule
-from luiti import luigi, TaskDay
+from luiti import luigi, TaskDay, manager
 
 
 class TestSensorSchedule(unittest.TestCase):
 
     def test_read_all_required_tasks(self):
-        SensorSchedule
-
-        from luiti import manager
         BetaReportDay = manager.load_a_task_by_name("BetaReportDay")
         ordered_task_instances = SensorSchedule.read_all_required_tasks(BetaReportDay(date_value="2014-09-01"))
-        result = map(str, ordered_task_instances)
-        self.assertEqual(result, ['DumpBrowserMapDay(date_value=2014-09-01T00:00:00+08:00)', 'DumpWebLogDay(date_value=2014-09-01T00:00:00+08:00)', 'CleanWebLogDay(date_value=2014-09-01T00:00:00+08:00)', 'CounterVisitorByBrowserDay(date_value=2014-09-01T00:00:00+08:00)', 'CounterVisitorByRegionDay(date_value=2014-09-01T00:00:00+08:00)', 'CounterVisitorDay(date_value=2014-09-01T00:00:00+08:00)', 'BetaReportDay(date_value=2014-09-01T00:00:00+08:00)'])
+        result = map(lambda i1: i1.task_clsname, ordered_task_instances)
+        self.assertEqual(result, ['DumpBrowserMapDay', 'DumpWebLogDay', 'CleanWebLogDay', 'CounterVisitorByBrowserDay', 'CounterVisitorByRegionDay', 'CounterVisitorDay', 'BetaReportDay'])
 
     def test_is_external(self):
         class ExampleExternalTask(luigi.ExternalTask):
