17622353413bec6d98a6b9da741e2b1fbc3c1023
David Chen <mvjome@gmail.com>
2015-07-18 20:22:47 +0800

    remove unneeded code

--- tests/test_mr_test_case.py
+++ tests/test_mr_test_case.py
@@ -6,15 +6,11 @@ RootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, RootDir)
 os.environ['LUIGI_CONFIG_PATH'] = RootDir + '/tests/client.cfg'
 
+from luiti.tests import SetupLuitiPackages
+config = SetupLuitiPackages.config
+
 import unittest
 from luiti import MrTestCase
-
-sys.path.insert(0, os.path.join(
-    RootDir, "tests/zip_package_by_luiti"))
-
-
-# change work dir
-os.chdir(os.path.join(RootDir, "tests/project_A"))
 
 
 @MrTestCase
