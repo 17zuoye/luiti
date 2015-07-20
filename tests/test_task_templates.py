dd67fc599a490706e721443ea0058722a79753cc
David Chen <mvjome@gmail.com>
2015-07-20 15:48:01 +0800

    let StaticFile use data_file too, and add test passed

--- tests/test_task_templates.py
+++ tests/test_task_templates.py
@@ -13,7 +13,7 @@ from luiti.tests import date_begin
 
 class TestLuitiUtils(unittest.TestCase):
 
-    def test_main(self):
+    def test_MongoImportTask(self):
         from luiti import MongoImportTask
 
         class AnotherMongoDay(MongoImportTask):
@@ -28,6 +28,19 @@ class TestLuitiUtils(unittest.TestCase):
         self.assertEqual(mongo_task.mongodb_connection_host, "192.168.20.111")
         self.assertEqual(mongo_task.mongodb_connection_port, 37001)
         self.assertEqual(mongo_task.mongoimport_command, "/usr/bin/mongoimport --host 192.168.20.111 --port 37001 --db 17zuoye_crm --collection teacher_report --file /foobar.json")
+
+    def test_StaticFile(self):
+        from luiti import StaticFile, luigi
+
+        class FoobarFileDay(StaticFile):
+            data_file = "/foobar"
+            IODevice = luigi.LocalTarget
+        self.assertEqual(FoobarFileDay().output().path, "/foobar")
+
+        class OldFoobarFileDay(StaticFile):
+            filepath = "/foobar"
+            IODevice = luigi.LocalTarget
+        self.assertEqual(OldFoobarFileDay().output().path, "/foobar")
 
 
 if __name__ == '__main__':
